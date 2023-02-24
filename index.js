6ac4c7ff14562dd40c1db4449180c0a6956e4423
PusanStudio <fangtham2545@gmail.com>
2023-02-24 12:37:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const axios = require('axios');
 async function run() {
   try {
     core.info(`get inputs ...`);
-    const url = core.getInput('portainer-url');
-    const api_key = core.getInput('portainer-api-key');
-    const endpoint = core.getInput('portainer-endpoint');
-    const stack = core.getInput('portainer-stack');
-    
+    const url = core.getInput('portainer-url', {required: true});
+    const api_key = core.getInput('portainer-api-key', {required: true});
+    const endpoint = parseInt(core.getInput('portainer-endpoint', {required: true}));
+    const stack = parseInt(core.getInput('portainer-stack', {required: true}));
+
     core.info(`get stack env ...`);
-    let stack_data = await axios({method: 'get',url: `${url}/api/stacks/${stack}`,headers: { 'X-API-Key': api_key}})
+    let stack_data = await axios({ method: 'get', url: `${url}/api/stacks/${stack}`, headers: { 'X-API-Key': api_key } })
 
     core.info(`get stack file ...`);
-    let stack_file = await axios({method: 'get',url: `${url}/api/stacks/${stack}/file`,headers: { 'X-API-Key': api_key}})
+    let stack_file = await axios({ method: 'get', url: `${url}/api/stacks/${stack}/file`, headers: { 'X-API-Key': api_key } })
 
-    core.info(`update stack & pull image ...`);
+    core.info(`update stack & repull image ...`);
     let update = await axios({
       method: 'put',
       url: `${url}/api/stacks/${stack}?endpointId=${endpoint}`,
-      headers: { 'X-API-Key': api_key, 'Content-Type': 'application/json'},
-      data : JSON.stringify({
+      headers: { 'X-API-Key': api_key, 'Content-Type': 'application/json' },
+      data: JSON.stringify({
         "StackFileContent": stack_file.data.StackFileContent,
         "Env": stack_data.data.Env,
         "Prune": false,
