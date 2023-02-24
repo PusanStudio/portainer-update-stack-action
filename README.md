b3b0b98cfb446779e0321b7adb1b3073bab38997
PusanStudio <fangtham2545@gmail.com>
2023-02-24 11:41:32 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# portainer-update-stack
+# portainer-update-stack-action
  Update Portainer Stack with Portainer-CE API
+
+## Example usage
+
+```yaml
+uses: PusanStudio/portainer-update-stack-action
+with:
+  portainer-url: ${{ secrets.PORTAINER_URL }}
+  portainer-api-key: ${{ secrets.PORTAINER_API_KEY }}
+  portainer-endpoint: 1
+  portainer-stack: 1
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+const axios = require('axios');
+
+async function run() {
+  try {
+    core.info(`get inputs ...`);
+    const url = core.getInput('portainer-url');
+    const api_key = core.getInput('portainer-api-key');
+    const endpoint = core.getInput('portainer-endpoint');
+    const stack = core.getInput('portainer-stack');
+    
+    core.info(`get stack env ...`);
+    let stack_data = await axios({method: 'get',url: `${url}/api/stacks/${stack}`,headers: { 'X-API-Key': api_key}})
+
+    core.info(`get stack file ...`);
+    let stack_file = await axios({method: 'get',url: `${url}/api/stacks/${stack}/file`,headers: { 'X-API-Key': api_key}})
+
+    core.info(`update stack & pull image ...`);
+    let update = await axios({
+      method: 'put',
+      url: `${host}/api/stacks/${stack}?endpointId=${endpoint}`,
+      headers: { 'X-API-Key': api_key, 'Content-Type': 'application/json'},
+      data : JSON.stringify({
+        "StackFileContent": stack_file.data.StackFileContent,
+        "Env": stack_data.data.Env,
+        "Prune": false,
+        "PullImage": true
+      })
+    })
+
+    core.setOutput('status', update.status);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
